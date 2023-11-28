a63a9c779b76ea0064d4a31d24babf1f92083d9b
mishasro72 <mishasro72@gmail.com>
2023-11-28 17:07:49 -0500

    Обновление теста в git

--- test_items.py
+++ test_items.py
@@ -4,10 +4,10 @@ import time
 
 BASE_URL = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_add_button(driver):
+def test_add_button_present(driver):
 
     driver.get(BASE_URL)
-    time.sleep(10)
+    #time.sleep(10)
     ADD_BUTTON = driver.find_element(By.XPATH, '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]')
     assert ADD_BUTTON.is_displayed()
 
